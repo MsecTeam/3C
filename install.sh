2a52b41ba1b9aad03e83de90860d551a86e60616
Msec <121532855+MsecTeam@users.noreply.github.com>
2023-05-27 22:49:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-cd /home/kali/go/bin
-sudo mv subfinder /usr/bin/
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-cd /home/kali/go/bin
-sudo mv httpx /usr/bin/
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
-cd /home/kali/go/bin
-sudo mv nuclei /usr/bin/
+sudo mv ~/go/bin/* /usr/bin/
 figlet Done
